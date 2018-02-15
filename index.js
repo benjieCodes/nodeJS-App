d25c89c1dd850f3a15854a3be29273d306726f00
Benjie <benjiecodes@gmail.com>
2018-02-15 15:31:19 -0500

    attempting to create routes

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+import {
+    log
+} from 'util';
+
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    console.log('hey');
+    res.send('Hey! It works!');
+    console.log(req);
+});
